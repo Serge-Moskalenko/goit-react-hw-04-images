1faded033fdae99f62fd5a0cbceffa72ab0ba7bc
Serge-Moskalenko <skmos2019@outlook.com>
2022-12-10 15:16:15 +0000

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,33 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+import React,{Component} from "react";
+import { AppStyled } from "./App.styled";
+import { ImageGallery } from "./ImageGallery/ImageGallery";
+import { Searchbar } from "./Searchbar/Searchbar";
+import { ApiServise } from "./ApiServise";
+
+const apiServise = new ApiServise();
+
+export class App extends Component{
+
+  state = {
+    images:null,
+  }
+
+  componentDidMount() {
+
+   apiServise.fetch().then(images =>this.setState({images}))
+  }
+
+  queryParameter = q => {
+    console.log(q)
+  }
+
+  render() {
+    return (
+    <AppStyled>
+        <Searchbar query={ this.queryParameter} />
+      <ImageGallery/>
+    </AppStyled>
   );
+  }
+  
 };
